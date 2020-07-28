f8e657989970a0c3ae8c8e31dfacce13bb6ebb25
Dalia Sawaya <dalia.sawaya@flatironschool.com>
2020-07-28 17:26:57 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,44 @@
+class Artist
+  
+@@all = []
+  attr_accessor :name
+  attr_writer :songs
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+  
+  def songs
+    Song.all.select {|song| song.artist == self}
+  end
+
+  def self.all
+    @@all    
+  end
+
+  def add_song(song)
+    song.artist = self
+  end
+
+  def self.find_or_create_by_name(name)
+    self.find(name) ? self.find(name) : self.new(name)
+    
+    #OR
+    # if self.all.find {|artist| artist.name == name }
+    #   self.all.find {|artist| artist.name == name }
+    # else
+    #   new_artist = Artist.new(name)
+    #   new_artist
+    # end
+  end
+
+  def self.find(name)
+    self.all.find {|artist| artist.name == name }
+  end
+
+  def print_songs
+    songs.each {|song| puts song.name}
+  end
+  
+end
