2f0502c6f43134fceaedb780d4ff8043c6435369
Sean Ransonette <sransonette@gmail.com>
2020-08-01 00:51:50 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,54 @@
+require 'pry'
+
+
+class Artist 
+  
+  attr_accessor :name
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    @songs = []
+    @@all << self
+    
+  end
+  
+  
+  def add_song(song)
+    song.artist = self
+    
+  end
+  
+  def add_song_by_name(song)
+    song = Song.new(song)
+    add_song(song)
+  end 
+  
+  def songs 
+    Song.all.select {|song| song.artist == self}
+  end
+  
+  def self.song_count
+    Song.all.length do |count|
+    end
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def self.find_or_create_by_name(song)
+  artist = self.all.find {|artist| artist.name == name}
+  if artist
+    artist 
+  else 
+    name = self.new(name)
+  end
+  
+    #binding.pry
+  end
+  
+  
+  
+end
