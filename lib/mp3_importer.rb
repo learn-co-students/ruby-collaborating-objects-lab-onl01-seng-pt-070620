78730eeb352df41f8f98af5643d1c99fc16b90d6
Andy Adams <andrewdcadams@gmail.com>
2020-08-04 13:25:07 +0000

    Done.

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -1,21 +1,28 @@
+require 'pry'
 class MP3Importer
   
-  
+  attr_accessor :file_path
   
   
   def initialize(file_path)
-    
+    @file_path = file_path
   end
   
   
   def files
+    Dir["#{@file_path}/**.mp3"].collect{ |file| file[21..-1]}
+  end
+  
+  def import
+    x = files
+    x.each { |file| Song.new_by_filename(file)}
     
   end
   
-  def import 
     
-  end
-  
+  def path
+    @file_path
+  end 
   
   
   
--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class Song 
   
   @@all = []
@@ -11,8 +12,24 @@ class Song
   def self.all 
     @@all 
   end
+
+  def self.new_by_filename(file_name)
+    string_array = file_name.split(" - ")
+    new_genre = string_array[2].delete_suffix('.mp3')
+    new_name = string_array[1]
+    new_artist = string_array[0]
+    
+    new_song = Song.new(new_name)
+    
+    new_artist_obj = Artist.find_or_create_by_name(new_artist)
+    new_song.artist = new_artist_obj
+    new_song
+    #binding.pry
+  end
   
-  
-  
+  def artist_name=(artist_name_string)
+     new_artist_obj = Artist.find_or_create_by_name(artist_name_string)
+     self.artist = new_artist_obj
+  end
   
 end 
