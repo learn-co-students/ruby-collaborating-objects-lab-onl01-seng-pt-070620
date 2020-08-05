4b22313f6101d7e8f531f156649ccadd36223334
Billy Gunn, Jr <bgunnjr@gmail.com>
2020-08-05 23:33:34 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,9 +1,12 @@
 class Song
 
   attr_accessor :name, :artist
+  
+  @@all = []
 
   def initialize(name)
     @name = name
+    @@all << self 
   end
 
   def self.new_by_filename(file_name)
@@ -17,6 +20,10 @@ class Song
   def artist_name=(name)
     self.artist = Artist.find_or_create_by_name(name)
     artist.add_song(self)
+  end
+  
+  def self.all 
+    @@all 
   end
 
 end
