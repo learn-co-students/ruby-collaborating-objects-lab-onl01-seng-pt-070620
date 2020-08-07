666398c5ee89a53ccd0369f8e579d582e8914548
john beltrez <beltrezjohn@gmail.com>
2020-08-07 08:46:43 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -12,7 +12,9 @@ class Artist
   def save 
     
   end 
-  def add_song(song)
+  def add_song(song) #s = Song.new("hotline_bling")
+  #drake = Artist.new("drake")
+  #drake.add_song("kiki")
     @songs << song
     song.artist= self
   end 
@@ -33,7 +35,7 @@ class Artist
       #my create by method returns the artist for me, both find_by_name and create_by_name are custom class methods, a contructor and a finder and we create them first and then use them to make a new method with both of their functionalities 
   end 
   def print_songs 
-    puts @songs.collect {|x| x.title}
+    puts @songs.collect {|x| x.name}
     #doesnt self have a property of songs?????
   end 
 end 
--- lib/song.rb
+++ lib/song.rb
@@ -1,6 +1,60 @@
+require 'pry'
 class Song 
-  attr_accessor :title, :artist
-  def initialize(title)
-    @title= title 
+  attr_accessor :name, :artist
+  @@all = []
+  def initialize(name)
+    @name= name
+    @@all << self
   end 
+  def self.all 
+    @@all 
+  end
+  def save 
+    self.class.all << self
+  end
+  def self.create
+    song = self.new 
+    song.save
+    song
+  end 
+  def self.new_by_name(name)
+    song = Song.new
+    song.name = name 
+    song 
+  end 
+  def self.create_by_name(name)
+    song = self.new 
+    song.name = name 
+    song.save
+    song
+  end 
+  def self.find_by_name(name)
+    all.find {|n| n.name == name}
+  end 
+  def self.find_or_create_by_name(name)
+    find_by_name(name) || create_by_name(name)
+  end 
+  def artist_name 
+    
+  end 
+  
+  def self.new_from_filename(filename)
+    artist, song = filename.split(" - ")
+    parsed_song = song
+    
+    new_song = self.new
+    new_song.name = parsed_song 
+    new_song.artist = artist 
+    new_song
+    binding.pry
+  end
+    
+  
+  def artist_name 
+  end 
+  
 end 
+#s = Song.new("99")
+#s.artist = artist_1
+
+# how to run the test for a single file 
