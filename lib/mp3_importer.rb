843fe127b108b15f6d099301642dd86369aafc73
john beltrez <beltrezjohn@gmail.com>
2020-08-07 09:58:05 +0000

    Done.

---
+++ lib/mp3_importer.rb
@@ -0,0 +1,11 @@
+class MP3Importer 
+  attr_accessor :path 
+  def initialize(path)
+    @path= path 
+  end 
+  def files
+  end 
+  def import 
+    Song.new_by_filename(some_filename)
+  end 
+end 
